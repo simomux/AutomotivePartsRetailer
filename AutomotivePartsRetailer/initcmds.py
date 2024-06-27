cf2fb2237ae86cfd0d1e7a6b621f5901a5ee7bbd
SIMONE MUSSINI <284900@studenti.unimore.it>
2024-06-27 16:50:50 +0200

    Created a small homepage. Created model for product and all the table that it uses. Added small .csv with some placeholders products to simulate a real application of the website and testing.
    Next I think I'll start creating some views for the products, and then I'll start working on the user and cart modules.

---
+++ AutomotivePartsRetailer/initcmds.py
@@ -0,0 +1,63 @@
+import os.path
+
+from products.models import Product, CarModel, CarMaker, Category, Country
+from csv import reader
+
+
+def erase_db():
+    print('Erasing DB')
+    Category.objects.all().delete()
+    Product.objects.all().delete()
+    CarMaker.objects.all().delete()
+    CarModel.objects.all().delete()
+    Country.objects.all().delete()
+
+
+def init_db():
+    print('Initializing DB')
+    if len(Category.objects.all()) != 0 and len(Country.objects.all()) != 0:
+        return
+
+    # Categories of listed items
+    list_categories = ["Spare part", "Tool", "Accessory"]
+
+    # Adding Categories to the DB
+    for i in list_categories:
+        cat = Category(name=i)
+        cat.save()
+
+    # Main countries of car manufacturers
+    with open("static/data/countries.csv") as manufacturers:
+        csv_reader = reader(manufacturers, delimiter=',')
+        for row in csv_reader:
+            country = Country(name=row[0])
+            country.save()
+
+    # Reading main car brands and pairing them to the respective countries
+    with open("static/data/manufacturers.csv") as manufacturers:
+        csv_reader = reader(manufacturers, delimiter=',')
+        for row in csv_reader:
+            country = Country.objects.get(name=row[1])
+            maker = CarMaker(name=row[0], country=country)
+            maker.save()
+
+    # Reading car models and pairing them to the respective manufacturers
+    with open("static/data/models.csv") as models:
+        csv_reader = reader(models, delimiter=',')
+        for row in csv_reader:
+            maker = CarMaker.objects.get(name=row[1])
+            car_model = CarModel(name=row[0], maker=maker)
+            car_model.save()
+
+    # Adding manually some products to the DB -- still in testing
+    with open("static/data/products.csv") as products:
+        csv_reader = reader(products, delimiter=',')
+        for i in csv_reader:
+            category = Category.objects.get(name=i[4].strip())
+            image = os.path.join("imgs/", i[0].replace(" ", "_")+".webp")
+            if len(i) > 5:
+                model = CarModel.objects.get(name=i[5].strip())
+                product = Product(name=i[0].strip(), description=i[1].strip(), price=i[2].strip(), stock=i[3].strip(), category=category, model=model, image=image)
+            else:
+                product = Product(name=i[0].strip(), description=i[1].strip(), price=i[2].strip(), stock=i[3].strip(), category=category, image=image)
+            product.save()
--- AutomotivePartsRetailer/urls.py
+++ AutomotivePartsRetailer/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .initcmds import init_db, erase_db
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('products.urls')),
 ]
+
+erase_db()
+init_db()
